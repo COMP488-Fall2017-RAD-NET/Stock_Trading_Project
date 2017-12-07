d485947986b797a0e6d5fd6360998c77a58bbf6a
andreifedorovforte <andrei.fedorov@fortegrp.net>
2017-12-07 11:05:24 -0600

    added mvc for the portfolio table

---
+++ WebApplication2/Controllers/PortfolioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            var entities = new stocktradingEntities();
+            return View(entities.Portfolios.ToList());
+        }
+    }
+}
