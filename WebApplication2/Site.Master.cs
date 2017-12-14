019a5c473e3624574695ff6043870ee46bfbef9d
andreifedorovforte <andrei.fedorov@fortegrp.net>
2017-12-13 22:41:30 -0600

    implemented logout

--- WebApplication2/Site.Master.cs
+++ WebApplication2/Site.Master.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -15,5 +17,12 @@ namespace WebApplication2
 
         }
 
+        protected void logout_button_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            HttpContext.Current.User = new GenericPrincipal(new GenericIdentity(string.Empty), null);
+            Session["authenticated"] = false;
+            Response.Redirect("/", true);
+        }
     }
 }
